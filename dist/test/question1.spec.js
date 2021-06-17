cd231f759894f8c80be234b92f68f52e78f9b2fa
duaifzn <duaifzn@gmail.com>
2021-06-17 22:12:00 +0800

    chore: modify test scripts

--- dist/test/question1.spec.js
+++ dist/test/question1.spec.js
@@ -1,18 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const question1_1 = require("../question1");
-(() => {
-    const { assert } = require('chai');
-    describe('add Comma test case', () => {
-        it('addComma(1234567) should be 1,234,567', () => {
-            assert.equal(question1_1.addComma('1234567'), '1,234,567');
-        });
-        it('addComma(-1234567) should be -1,234,567', () => {
-            assert.equal(question1_1.addComma('-1234567'), '-1,234,567');
-        });
-        it('addComma(1234567.12345) should be 1,234,567.12345', () => {
-            assert.equal(question1_1.addComma('1234567.12345'), '1,234,567.12345');
-        });
+const chai_1 = require("chai");
+describe('addComma() test case', () => {
+    it('addComma(1234567) should be 1,234,567', () => {
+        chai_1.assert.equal(question1_1.addComma('1234567'), '1,234,567');
     });
-})();
+    it('addComma(-1234567) should be -1,234,567', () => {
+        chai_1.assert.equal(question1_1.addComma('-1234567'), '-1,234,567');
+    });
+    it('addComma(1234567.12345) should be 1,234,567.12345', () => {
+        chai_1.assert.equal(question1_1.addComma('1234567.12345'), '1,234,567.12345');
+    });
+});
 //# sourceMappingURL=question1.spec.js.map
--- src/test/question1.spec.ts
+++ src/test/question1.spec.ts
@@ -1,15 +1,15 @@
 import {addComma} from '../question1';
-(() => {
-  const {assert} = require('chai');
-  describe('addComma() test case', () => {
-    it('addComma(1234567) should be 1,234,567', () => {
-      assert.equal(addComma('1234567'), '1,234,567');
-    });
-    it('addComma(-1234567) should be -1,234,567', () => {
-      assert.equal(addComma('-1234567'), '-1,234,567');
-    });
-    it('addComma(1234567.12345) should be 1,234,567.12345', () => {
-      assert.equal(addComma('1234567.12345'), '1,234,567.12345');
-    });
+import { assert } from 'chai';
+
+describe('addComma() test case', () => {
+  it('addComma(1234567) should be 1,234,567', () => {
+    assert.equal(addComma('1234567'), '1,234,567');
   });
-})();
+  it('addComma(-1234567) should be -1,234,567', () => {
+    assert.equal(addComma('-1234567'), '-1,234,567');
+  });
+  it('addComma(1234567.12345) should be 1,234,567.12345', () => {
+    assert.equal(addComma('1234567.12345'), '1,234,567.12345');
+  });
+});
+
